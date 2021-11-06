fea1546a8b167c5b7f6d54e16abfe048c75c92c3
Carl Huffmeier <carlhueffmeier@gmail.com>
2021-11-06 20:35:48 +0800

    feat: Add traceMethod wrapper

--- src/decorator.ts
+++ src/decorator.ts
@@ -1,9 +1,11 @@
 import { SpanStatusCode, Tracer } from '@opentelemetry/api';
 
-export function TraceMethod(options: {
+export interface TraceMethodOptions {
   tracer: Tracer;
   spanName?: string;
-}): MethodDecorator {
+}
+
+export function TraceMethod(options: TraceMethodOptions): MethodDecorator {
   const { tracer } = options;
 
   return function traceMethodDecorator(
@@ -16,49 +18,62 @@ export function TraceMethod(options: {
       options.spanName ??
       [target?.constructor?.name, String(propertyKey)].join('#');
 
-    descriptor.value = function (...args: any[]) {
-      return tracer.startActiveSpan(spanName, (span) => {
-        try {
-          const returnValue = originalMethod.apply(this, args);
-          if (typeof returnValue.then === 'function') {
-            return returnValue
-              .then((resolvedValue: unknown) => {
-                span.setStatus({ code: SpanStatusCode.OK });
+    descriptor.value = traceMethod({ tracer, spanName }, originalMethod);
+    Object.defineProperty(descriptor.value, 'name', { value: propertyKey });
+  };
+}
 
-                return resolvedValue;
-              })
-              .catch((error: any) => {
-                span.recordException(error);
-                span.setStatus({
-                  code: SpanStatusCode.ERROR,
-                  message: error.message,
-                });
+export function traceMethod<F extends (...args: A) => any, A extends any[]>(
+  options: TraceMethodOptions,
+  fn: F,
+): F {
+  const { tracer } = options;
+  const spanName = options.spanName ?? fn.name;
+  if (!spanName) {
+    throw new Error('Specify spanName for anonymous function!');
+  }
 
-                throw error;
-              })
-              .finally(() => span.end());
-          } else {
-            span.setStatus({ code: SpanStatusCode.OK });
-            span.end();
+  const decoratedMethod = function (this: any, ...args: A): any {
+    return tracer.startActiveSpan(spanName, (span) => {
+      try {
+        const returnValue = fn.apply(this, args);
+        if (typeof returnValue.then === 'function') {
+          return returnValue
+            .then((resolvedValue: unknown) => {
+              span.setStatus({ code: SpanStatusCode.OK });
 
-            return returnValue;
-          }
-        } catch (error: any) {
-          span.recordException(error);
-          span.setStatus({
-            code: SpanStatusCode.ERROR,
-            message: error.message,
-          });
+              return resolvedValue;
+            })
+            .catch((error: any) => {
+              span.recordException(error);
+              span.setStatus({
+                code: SpanStatusCode.ERROR,
+                message: error.message,
+              });
+
+              throw error;
+            })
+            .finally(() => span.end());
+        } else {
+          span.setStatus({ code: SpanStatusCode.OK });
           span.end();
 
-          throw error;
+          return returnValue;
         }
-      });
-    };
+      } catch (error: any) {
+        span.recordException(error);
+        span.setStatus({
+          code: SpanStatusCode.ERROR,
+          message: error.message,
+        });
+        span.end();
 
-    // Set the method name to the original method name
-    Object.defineProperty(descriptor.value, 'name', {
-      value: propertyKey,
+        throw error;
+      }
     });
   };
+
+  Object.defineProperty(decoratedMethod, 'name', { value: fn.name });
+
+  return decoratedMethod as F;
 }
--- test/decorator.spec.ts
+++ test/decorator.spec.ts
@@ -1,5 +1,8 @@
 import { Span, SpanStatusCode, Tracer } from '@opentelemetry/api';
-import { TraceMethod } from '../src';
+import { TraceMethod, traceMethod } from '../src';
+
+// TODO: Test passing a name
+// TODO: Test `this` value with class decorator
 
 const mockTracer: jest.Mocked<Tracer> = {
   startSpan: jest.fn(),
@@ -124,6 +127,153 @@ describe('@TraceMethod decorator', () => {
     const expectedErrorMessage = 'expected error message';
 
     await expect(target.asyncThrow(expectedErrorMessage)).rejects.toEqual(
+      new Error(expectedErrorMessage),
+    );
+
+    expect(mockSpan.recordException).toHaveBeenLastCalledWith(
+      new Error(expectedErrorMessage),
+    );
+    expect(mockSpan.setStatus).toHaveBeenCalledWith({
+      code: SpanStatusCode.ERROR,
+      message: expectedErrorMessage,
+    });
+    expect(mockSpan.end).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('traceMethod function wrapper', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    mockTracer.startActiveSpan.mockImplementation((name: string, fn: any) =>
+      fn(mockSpan),
+    );
+  });
+
+  it('should return function value, given no error', () => {
+    const syncAdd = traceMethod(
+      { tracer: mockTracer },
+      function syncAdd(a: number, b: number) {
+        return a + b;
+      },
+    );
+
+    const result = syncAdd(1, 1);
+
+    expect(result).toBe(2);
+  });
+
+  it('should use correct context, given invocation with context', () => {
+    const returnName = traceMethod(
+      { tracer: mockTracer },
+      function returnName(this: { name: string }): string {
+        return this.name;
+      },
+    );
+
+    const result = returnName.call({ name: 'carl' });
+
+    expect(result).toBe('carl');
+  });
+
+  it('should set correct span status, given no error', () => {
+    const syncAdd = traceMethod(
+      { tracer: mockTracer },
+      function syncAdd(a: number, b: number) {
+        return a + b;
+      },
+    );
+
+    syncAdd(1, 1);
+
+    expect(mockSpan.setStatus).toHaveBeenCalledWith({
+      code: SpanStatusCode.OK,
+    });
+    expect(mockSpan.setStatus).toHaveBeenCalledTimes(1);
+    expect(mockSpan.end).toHaveBeenCalledTimes(1);
+  });
+
+  it('should set correct span name, given no name argument', () => {
+    const syncAdd = traceMethod(
+      { tracer: mockTracer },
+      function syncAdd(a: number, b: number) {
+        return a + b;
+      },
+    );
+
+    syncAdd(1, 1);
+
+    expect(mockTracer.startActiveSpan).toHaveBeenCalledWith(
+      'syncAdd',
+      expect.any(Function),
+    );
+  });
+
+  it('should throw error, given exception in sync method', () => {
+    const expectedErrorMessage = 'expected error message';
+    const syncThrow = traceMethod({ tracer: mockTracer }, function syncThrow() {
+      throw new Error(expectedErrorMessage);
+    });
+
+    expect(() => syncThrow()).toThrow(new Error(expectedErrorMessage));
+  });
+
+  it('should record error, given exception in sync method', () => {
+    const expectedErrorMessage = 'expected error message';
+    const syncThrow = traceMethod({ tracer: mockTracer }, function syncThrow() {
+      throw new Error(expectedErrorMessage);
+    });
+
+    expect(() => syncThrow()).toThrow();
+    expect(mockSpan.recordException).toHaveBeenLastCalledWith(
+      new Error(expectedErrorMessage),
+    );
+    expect(mockSpan.setStatus).toHaveBeenCalledWith({
+      code: SpanStatusCode.ERROR,
+      message: expectedErrorMessage,
+    });
+    expect(mockSpan.end).toHaveBeenCalledTimes(1);
+  });
+
+  it('should resolve correctly, given async method', async () => {
+    const asyncAdd = traceMethod(
+      { tracer: mockTracer },
+      async function asyncAdd(a: number, b: number): Promise<number> {
+        return a + b;
+      },
+    );
+
+    const result = await asyncAdd(1, 1);
+
+    expect(result).toBe(2);
+  });
+
+  it('should set span status correctly, given async method without error', async () => {
+    const asyncAdd = traceMethod(
+      { tracer: mockTracer },
+      async function asyncAdd(a: number, b: number): Promise<number> {
+        return a + b;
+      },
+    );
+
+    await asyncAdd(1, 1);
+
+    expect(mockSpan.setStatus).toHaveBeenCalledWith({
+      code: SpanStatusCode.OK,
+    });
+    expect(mockSpan.setStatus).toHaveBeenCalledTimes(1);
+    expect(mockSpan.end).toHaveBeenCalledTimes(1);
+  });
+
+  it('should record error, given exception in async method', async () => {
+    const expectedErrorMessage = 'expected error message';
+    const asyncThrow = traceMethod(
+      { tracer: mockTracer },
+      async function asyncThrow(): Promise<void> {
+        throw new Error(expectedErrorMessage);
+      },
+    );
+
+    await expect(asyncThrow()).rejects.toEqual(
       new Error(expectedErrorMessage),
     );
 
